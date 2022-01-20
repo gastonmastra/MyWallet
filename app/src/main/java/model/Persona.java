be2f4e027abd6259973560f2d20f6c1c9bd2badc
gastonmastra <gastimastra@gmail.com>
2022-01-20 11:12:23 -0300

    first commit

---
+++ app/src/main/java/model/Persona.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Persona {
+
+    public String nombre;
+    public String apellido;
+    public String uid;
+    public String correo;
+
+    public Persona() {
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
